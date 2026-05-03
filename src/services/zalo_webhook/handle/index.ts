c7285073b86e26b5d90170280b199778b58c87ba
La <laduchai20111995@gmail.com>
2026-05-03 20:34:57 +0700

    token callback begin

--- src/services/zalo_webhook/handle/index.ts
+++ src/services/zalo_webhook/handle/index.ts
@@ -4,17 +4,14 @@ import { HookDataField } from '@src/dataStruct/hookData';
 import { sendHookData } from '@src/messageQueue/Producer';
 import { getEnv } from '@src/mode';
 import { myEnv } from '@src/mode/type';
-
+import axios from 'axios';
 
 const VERIFY_TOKEN = process.env.ZALO_VERIFY_TOKEN!;
 const prefix = getEnv() === myEnv.Dev ? '_dev' : '';
 
-
 class Handle_Zalo_WebHook {
-
     getData = async (req: Request, res: Response) => {
-        
-        console.log('Zalo_WebHook', 'getData', req.query)
+        console.log('Zalo_WebHook', 'getData', req.query);
         const { verify_token } = req.query;
 
         if (verify_token === VERIFY_TOKEN) {
@@ -22,18 +19,55 @@ class Handle_Zalo_WebHook {
             return;
         }
 
-        res.status(403).send("Invalid verify token");
+        res.status(403).send('Invalid verify token');
         return;
     };
 
     postData = (req: Request<unknown, unknown, HookDataField<unknown>>, res: Response) => {
-        console.log("Zalo Webhook Event:", req.body);
+        console.log('Zalo Webhook Event:', req.body);
         const hookDataBody = req.body as HookDataField;
 
-        sendHookData(`zalo_hook_data_queue${prefix}`, hookDataBody)
+        sendHookData(`zalo_hook_data_queue${prefix}`, hookDataBody);
 
         res.status(200).json({ received: true });
         return;
+    };
+
+    tokenCallback = async (req: Request, res: Response) => {
+        const code = req.query.code as string;
+
+        if (!code) {
+            res.send('No code');
+            return;
+        }
+
+        try {
+            const tokenRes = await axios.post(
+                'https://oauth.zaloapp.com/v4/access_token',
+                new URLSearchParams({
+                    app_id: process.env.ZALO_APP_ID!,
+                    code,
+                    grant_type: 'authorization_code',
+                }),
+                {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        secret_key: process.env.ZALO_SECRET_KEY!,
+                    },
+                }
+            );
+
+            const data = tokenRes.data;
+
+            console.log('TOKEN:', data);
+
+            res.json(data);
+            return;
+        } catch (err: any) {
+            console.error(err.response?.data || err.message);
+            res.send('Error lấy token');
+            return;
+        }
     };
 }
 
